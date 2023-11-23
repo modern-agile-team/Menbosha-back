6fdc9dcbf4a8605cd1867a354336f31e9722f72e
2swo <swlee456@naver.com>
2023-11-23 21:53:13 +0900

    add(#151) User_Intro entity 추가

---
+++ src/users/entities/user-intro.entity.ts
@@ -0,0 +1,32 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from './user.entity';
+
+@Entity({ name: 'user_intro' })
+export class UserIntro {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: 'user_id' })
+  userId: number;
+
+  @Column({ name: 'main_field' })
+  mainField: string;
+
+  @Column({ name: 'interuduce' })
+  interuduce: string;
+
+  @Column({ name: 'career' })
+  career: string;
+
+  @OneToOne(() => User, (userId: User) => userId.userImage, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+}
