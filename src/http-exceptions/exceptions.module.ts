fdfb17c2f8bf4a179bdc209ab2cd04ac549b315c
hobiJeong <jjb26433@naver.com>
2023-11-30 21:21:35 +0900

    feat(#10): useGlobalFilter(BadRequestException)

--- src/http-exceptions/exceptions.module.ts
+++ src/http-exceptions/exceptions.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { HttpExceptionService } from './services/http-exception.service';
+import { HttpBadRequestExceptionFilter } from './exception-filters/http-bad-request.exception.filter';
 
 @Module({
-  providers: [HttpExceptionService],
+  providers: [HttpExceptionService, HttpBadRequestExceptionFilter],
 })
 export class ExceptionsModule {}
