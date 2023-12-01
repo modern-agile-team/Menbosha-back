d87af703428bec0e224c4d28867ba68a3dc3cfc7
hobiJeong <jjb26433@naver.com>
2023-12-01 14:26:16 +0900

    feat(#10): apply Http-Forbidden-exception.filter

--- src/http-exceptions/exceptions.module.ts
+++ src/http-exceptions/exceptions.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { HttpExceptionService } from './services/http-exception.service';
 import { HttpBadRequestExceptionFilter } from './exception-filters/http-bad-request.exception.filter';
 import { HttpUnauthorizedExceptionFilter } from './exception-filters/http-unauthorized.exception.filter';
+import { HttpForbiddenExceptionFilter } from './exception-filters/http-forbidden.exception.filter';
 
 @Module({
   providers: [
     HttpExceptionService,
     HttpBadRequestExceptionFilter,
     HttpUnauthorizedExceptionFilter,
+    HttpForbiddenExceptionFilter,
   ],
 })
 export class ExceptionsModule {}
