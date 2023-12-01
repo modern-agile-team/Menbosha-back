27bdd4e3500ddafb0499148cc45e1bf6375932fc
hobiJeong <jjb26433@naver.com>
2023-12-01 14:21:47 +0900

    feat(#10): add HttpUnauthorized exception filter

--- src/http-exceptions/exceptions.module.ts
+++ src/http-exceptions/exceptions.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { HttpExceptionService } from './services/http-exception.service';
 import { HttpBadRequestExceptionFilter } from './exception-filters/http-bad-request.exception.filter';
+import { HttpUnauthorizedExceptionFilter } from './exception-filters/http-unauthorized.exception.filter';
 
 @Module({
-  providers: [HttpExceptionService, HttpBadRequestExceptionFilter],
+  providers: [
+    HttpExceptionService,
+    HttpBadRequestExceptionFilter,
+    HttpUnauthorizedExceptionFilter,
+  ],
 })
 export class ExceptionsModule {}
