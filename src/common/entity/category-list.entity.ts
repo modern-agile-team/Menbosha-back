cd9e914c3c41a8a789384e5e54d47c6c3a89d275
2swo <swlee456@naver.com>
2023-12-06 11:39:31 +0900

    refactor(#1) category-list.entity 수정

--- src/common/entity/category-list.entity.ts
+++ src/common/entity/category-list.entity.ts
@@ -1,10 +1,4 @@
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  OneToOne,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { User } from '../../users/entities/user.entity';
 import { HelpMeBoard } from 'src/boards/entities/help-me-board.entity';
 import { MentorBoard } from 'src/boards/entities/mentor-board.entity';
@@ -18,14 +12,11 @@ export class CategoryList {
   categoryName: string;
 
   @OneToOne(() => User, (user) => user.categoryList)
-  @JoinColumn({ name: 'user_id' })
   user: User;
 
   @OneToOne(() => HelpMeBoard, (helpMeBoard) => helpMeBoard.categoryList)
-  @JoinColumn({ name: 'help_me_board' })
   helpMeBoard: HelpMeBoard;
 
   @OneToOne(() => MentorBoard, (mentorBoard) => mentorBoard.categoryList)
-  @JoinColumn({ name: 'mentor_board' })
   mentorBoard: MentorBoard;
 }
