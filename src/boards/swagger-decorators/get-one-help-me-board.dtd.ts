ef530425abf25e66a10c5ad3f097ba5fc114af1f
2swo <swlee456@naver.com>
2023-12-14 23:43:08 +0900

    Modify(#35) HelpMeBoard setting

---
+++ src/boards/swagger-decorators/get-one-help-me-board.dtd.ts
@@ -0,0 +1,74 @@
+import { applyDecorators } from '@nestjs/common';
+import { ApiHeaders, ApiOperation, ApiResponse } from '@nestjs/swagger';
+
+export function ApiGetOneHelpMeBoard() {
+  return applyDecorators(
+    ApiOperation({
+      summary: '클릭한 멘티 보드 가져오는 API',
+      description: '클릭한 멘티 보드 가져오는 API',
+    }),
+    ApiResponse({
+      status: 200,
+      description: '성공적으로 멘티보드를 불러온 경우',
+      content: {
+        JSON: { example: { message: '멘티보드를 성공적으로 불러왔습니다.' } },
+      },
+    }),
+    ApiResponse({
+      status: 401,
+      description: '우리 서비스의 액세스 토큰이 아닌 경우',
+      content: {
+        JSON: {
+          example: { statusCode: 401, message: '유효하지 않은 토큰입니다.' },
+        },
+      },
+    }),
+    ApiResponse({
+      status: 403,
+      description: '만료된 액세스 토큰인 경우',
+      content: {
+        JSON: {
+          example: { statusCode: 403, message: '만료된 토큰입니다.' },
+        },
+      },
+    }),
+    ApiResponse({
+      status: 404,
+      description: 'DB에서 일치하는 보드를 찾을 수 없는 경우',
+      content: {
+        JSON: {
+          example: { statusCode: 404, message: '보드를 찾을 수 없습니다.' },
+        },
+      },
+    }),
+    ApiResponse({
+      status: 411,
+      description: '액세스 토큰이 제공되지 않은 경우',
+      content: {
+        JSON: {
+          example: { statusCode: 411, message: '토큰이 제공되지 않았습니다.' },
+        },
+      },
+    }),
+    ApiResponse({
+      status: 500,
+      description: '보드를 불러오는 중 오류가 발생한 경우',
+      content: {
+        JSON: {
+          example: {
+            statusCode: 500,
+            message: '보드를 불러오는 중 오류가 발생했습니다.',
+          },
+        },
+      },
+    }),
+    ApiHeaders([
+      {
+        name: 'access_token',
+        description: '액세스 토큰',
+        required: true,
+        example: '여기에 액세스 토큰',
+      },
+    ]),
+  );
+}
