d07a0f65e420418fe007d4ffd08daaab3adf97fb
hobiJeong <jjb26433@naver.com>
2023-12-14 16:09:25 +0900

    modify(#26): modify chat schema

--- src/chat/schemas/chat.schemas.ts
+++ src/chat/schemas/chat.schemas.ts
@@ -1,6 +1,5 @@
 import { Prop, Schema, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
-import { IsBoolean, IsMongoId, IsNumber, IsString } from 'class-validator';
 
 const options: SchemaOptions = {
   collection: 'Chat',
@@ -9,23 +8,18 @@ const options: SchemaOptions = {
 
 @Schema(options)
 export class Chat {
-  @IsMongoId()
   @Prop({ type: mongoose.Types.ObjectId, ref: 'ChatRoom' })
   chatroom_id: mongoose.Types.ObjectId;
 
-  @IsNumber()
   @Prop({ required: true })
   sender: number;
 
-  @IsNumber()
   @Prop({ required: true })
   receiver: number;
 
-  @IsString()
   @Prop({ required: true })
   content: string;
 
-  @IsBoolean()
   @Prop({ required: true, default: false })
   isSeen: boolean;
 }
