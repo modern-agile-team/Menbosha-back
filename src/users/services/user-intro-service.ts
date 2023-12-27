9062476c7a01670670f64a9ba25ee47fd70aecb7
2swo <swlee456@naver.com>
2023-12-27 17:36:56 +0900

    feat(#74) user_intro DTO

--- src/users/services/user-intro-service.ts
+++ src/users/services/user-intro-service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { UserIntroRepository } from '../repositories/user-intro-repositories';
 import { UserIntro } from '../entities/user-intro.entity';
+import { CreateUserIntroDto } from '../dtos/create-user-intro-dto';
 
 @Injectable()
 export class UserIntroService {
   constructor(private readonly userIntroRepository: UserIntroRepository) {}
 
-  async addUserIntro(userId: number): Promise<UserIntro> {}
+  async addUserIntro(
+    userId: number,
+    userData: CreateUserIntroDto,
+  ): Promise<UserIntro> {
+    return await this.userIntroRepository.createUserIntro(userId, userData);
+  }
 }
