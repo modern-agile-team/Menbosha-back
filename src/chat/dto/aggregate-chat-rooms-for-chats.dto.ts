1ea24b48572de2e79a2528f6c0e9bbefe4ecf82b
hobiJeong <jjb26433@naver.com>
2023-12-28 15:22:23 +0900

    feat(#40): create dto for chats

--- src/chat/dto/aggregate-chat-rooms-for-chats.dto.ts
+++ src/chat/dto/aggregate-chat-rooms-for-chats.dto.ts
@@ -1,1 +1,15 @@
-export class AggregateChatRoomsForChatsDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { ChatRoomsDto } from './chat-rooms.dto';
+
+export class AggregateChatRoomsForChatsDto extends ChatRoomsDto {
+  @ApiProperty({
+    description: '해당 채팅방 내의 채팅 총 개수.',
+  })
+  chatsCount: number;
+
+  constructor(aggregateChatRoomsForChatsDto: AggregateChatRoomsForChatsDto) {
+    super();
+
+    this.chatsCount = aggregateChatRoomsForChatsDto.chatsCount;
+  }
+}
