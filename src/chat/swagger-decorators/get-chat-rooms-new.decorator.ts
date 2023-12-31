da31a13299b44732f1c72ac61042900fb4b67c76
hobiJeong <jjb26433@naver.com>
2023-12-31 19:45:36 +0900

    modify(#40): modfiy get chat rooms new swagger api docs

--- src/chat/swagger-decorators/get-chat-rooms-new.decorator.ts
+++ src/chat/swagger-decorators/get-chat-rooms-new.decorator.ts
@@ -12,7 +12,8 @@ import { ResponseGetChatRoomsDto } from '../dto/response-get-chat-rooms.dto';
 export function ApiGetChatRoomsNew() {
   return applyDecorators(
     ApiOperation({
-      summary: '채팅룸 조회 새로운 api',
+      summary:
+        '챗룸에 유저 정보를 매핑하고 최신의 챗 1개만 가져오도록 하는 api',
       description: 'Header - user-token',
     }),
     ApiResponse({
@@ -23,7 +24,7 @@ export function ApiGetChatRoomsNew() {
             example: 200,
             type: 'number',
           },
-          data: {
+          content: {
             type: 'array',
             items: {
               $ref: getSchemaPath(ResponseGetChatRoomsDto),
@@ -33,14 +34,41 @@ export function ApiGetChatRoomsNew() {
       },
       description: '성공적으로 채팅방 조회',
     }),
+    ApiResponse({
+      status: 401,
+      description: '우리 서비스의 액세스 토큰이 아닌 경우',
+      content: {
+        JSON: {
+          example: { statusCode: 401, message: '유효하지 않은 토큰입니다.' },
+        },
+      },
+    }),
+    ApiResponse({
+      status: 403,
+      description: '만료된 액세스 토큰인 경우',
+      content: {
+        JSON: {
+          example: { statusCode: 403, message: '만료된 토큰입니다.' },
+        },
+      },
+    }),
     ApiNotFoundResponse({
       description: '내 정보를 찾을 수 없는 경우',
       schema: {
         type: 'object',
         example: {
           statusCode: 404,
-          message: '사용자를 찾을 수 없습니다.',
+          message: ['사용자를 찾을 수 없습니다.'],
           error: 'Not Found',
+        },
+      },
+    }),
+    ApiResponse({
+      status: 411,
+      description: '액세스 토큰이 제공되지 않은 경우',
+      content: {
+        JSON: {
+          example: { statusCode: 411, message: '토큰이 제공되지 않았습니다.' },
         },
       },
     }),
