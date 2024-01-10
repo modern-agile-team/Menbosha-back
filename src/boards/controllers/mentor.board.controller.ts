89f824cd99c0e60bb2539a693d39ec2de2d0ab41
2swo <swlee456@naver.com>
2024-01-10 16:46:56 +0900

    feat(#99) feat decorators

--- src/boards/controllers/mentor.board.controller.ts
+++ src/boards/controllers/mentor.board.controller.ts
@@ -31,6 +31,7 @@ import { FilesInterceptor } from '@nestjs/platform-express';
 import { CreateMentorBoardImageDto } from '../dto/mentorBoard/create.mentor.board.image.dto';
 import { ApiUploadMentorBoardImages } from '../swagger-decorators/mentorBoard/add-mentor-board-images-decorator';
 import { ApiGetPageNumberByMentorBoard } from '../swagger-decorators/mentorBoard/get-page-number-mentor-board-decorator';
+import { ApiGetRandomMentorBoards } from '../swagger-decorators/mentorBoard/get-random-mentor-boards-decorator';
 
 @Controller('mentor-board')
 @ApiTags('mentor-board API')
@@ -76,7 +77,8 @@ export class MentorBoardController {
     return this.mentorBoardService.findPagedMentorBoards(page, categoryId);
   }
 
-  @Get('random')
+  @Get('random') //랜덤한 멘토게시글 3개만 뽑아오기
+  @ApiGetRandomMentorBoards()
   randomMentorBoards() {
     return this.mentorBoardService.randomMentorBoards();
   }
---
+++ src/boards/swagger-decorators/mentorBoard/get-random-mentor-boards-decorator.ts
@@ -0,0 +1,86 @@
+import { applyDecorators } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+
+export function ApiGetRandomMentorBoards() {
+  return applyDecorators(
+    ApiOperation({
+      summary: '랜덤한 멘토 보드 가져오는 API',
+      description: '랜덤한 멘토 보드 3개 가져오는 API ',
+    }),
+    ApiResponse({
+      status: 200,
+      description: '성공적으로 랜덤한 멘토보드를 불러온 경우',
+      content: {
+        JSON: {
+          example: {
+            data: [
+              {
+                id: 'number',
+                head: 'string',
+                body: 'string',
+                createdAt: '2023-12-10T20:14:13.311Z',
+                updatedAt: '2023-12-10T20:14:13.311Z',
+                categoryId: 'number',
+                user: {
+                  name: '이승우',
+                  userImage: {
+                    id: 'number',
+                    userId: 'number',
+                    imageUrl: 'string ',
+                  },
+                },
+                mentorBoardImage: ['있을 경우 배열, 없을 경우 빈 배열'],
+              },
+              {
+                id: 23,
+                head: 'string',
+                body: 'string',
+                createdAt: '2023-12-22T05:09:12.142Z',
+                updatedAt: '2023-12-22T05:09:12.142Z',
+                categoryId: 'number',
+                user: {
+                  name: '이승우',
+                  userImage: {
+                    id: 'number',
+                    userId: 'number',
+                    imageUrl: 'string',
+                  },
+                },
+                mentorBoardImage: ['있을 경우 배열, 없을 경우 빈 배열'],
+              },
+              {
+                id: 20,
+                head: '게시물 제목',
+                body: 'click send to get a response b',
+                createdAt: '2023-12-19T20:55:53.285Z',
+                updatedAt: '2023-12-19T20:55:53.285Z',
+                categoryId: 'number',
+                user: {
+                  name: '이승우',
+                  userImage: {
+                    id: 14,
+                    userId: 'number',
+                    imageUrl: 'string',
+                  },
+                },
+                mentorBoardImage: ['있을 경우 배열, 없을 경우 빈 배열'],
+              },
+            ],
+          },
+        },
+      },
+    }),
+    ApiResponse({
+      status: 500,
+      description: '보드를 불러오는 중 오류가 발생한 경우',
+      content: {
+        JSON: {
+          example: {
+            statusCode: 500,
+            message: '보드를 불러오는 중 오류가 발생했습니다.',
+          },
+        },
+      },
+    }),
+  );
+}
