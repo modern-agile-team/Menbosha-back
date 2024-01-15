66b0f69014585dc409bc063be41cfa50e83e03c0
hobiJeong <jjb26433@naver.com>
2024-01-16 00:00:56 +0900

    feat(#94): extends query dto

---
+++ src/boards/constants/mentor-board-order-field.enum.ts
@@ -0,0 +1,9 @@
+export enum MentorBoardOrderField {
+  id = 'id',
+  userId = 'userId',
+  head = 'head',
+  createdAt = 'createdAt',
+  updatedAt = 'updatedAt',
+  categoryListId = 'categoryListId',
+  popularAt = 'popularAt',
+}
---
+++ src/boards/dto/mentorBoard/mentor-board-page-query.dto.ts
@@ -0,0 +1,26 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+
+import { IsEnum, IsOptional } from 'class-validator';
+import { MentorBoardOrderField } from 'src/boards/constants/mentor-board-order-field.enum';
+import { SortOrder } from '../../../common/constants/sort-order.enum';
+import { PageQueryDto } from 'src/common/dto/page-query.dto';
+
+export class MentorBoardPageQueryDto extends PageQueryDto {
+  @ApiPropertyOptional({
+    description: '정렬의 기준으로 잡을 필드',
+    enum: MentorBoardOrderField,
+    default: MentorBoardOrderField.id,
+  })
+  @IsOptional()
+  @IsEnum(MentorBoardOrderField)
+  orderField?: MentorBoardOrderField = MentorBoardOrderField.id;
+
+  @ApiPropertyOptional({
+    description: '오름차순 혹은 내림차순',
+    enum: SortOrder,
+    default: SortOrder.Asc,
+  })
+  @IsOptional()
+  @IsEnum(SortOrder)
+  SortOrder?: SortOrder = SortOrder.Asc;
+}
