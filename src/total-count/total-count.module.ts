29ff69334a85381e5eaacd14723110caae81a43f
NicoDora <pjh76574734@gmail.com>
2024-01-15 17:43:33 +0900

    fix(#92): 순환 종속성 문제 해결

--- src/total-count/total-count.module.ts
+++ src/total-count/total-count.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TotalCountRepository } from './repositories/total-count.repository';
 import { TotalCountService } from './services/total-count.service';
+import { TotalCountController } from './controllers/total-count.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [forwardRef(() => AuthModule)],
+  controllers: [TotalCountController],
   providers: [TotalCountRepository, TotalCountService],
   exports: [TotalCountRepository, TotalCountService],
 })
