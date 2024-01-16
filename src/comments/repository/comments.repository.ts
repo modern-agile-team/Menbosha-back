4b3b92b902b28af8741497fe338720b52e25fe5e
2swo <swlee456@naver.com>
2024-01-16 15:05:26 +0900

    Modify(#105) 디자인팀 기능삭제 요청

--- src/comments/repository/comments.repository.ts
+++ src/comments/repository/comments.repository.ts
@@ -36,22 +36,23 @@ export class CommentsRepository {
     });
   }
 
-  async updateComment(
-    id: number,
-    commentData: Partial<CreateCommentDto>,
-  ): Promise<HelpYouComment> {
-    const existingComment = await this.entityManager.findOne(HelpYouComment, {
-      relations: ['user', 'user.userImage'],
-      where: { id },
-    });
-    for (const key in commentData) {
-      if (commentData.hasOwnProperty(key)) {
-        existingComment[key] = commentData[key];
-      }
-    }
-    await this.entityManager.save(HelpYouComment, existingComment);
-    return existingComment;
-  }
+  // 이부분은 디자인팀의 요청에 의해 쓰이지 않는 API입니다(논의중)
+  // async updateComment(
+  //   id: number,
+  //   commentData: Partial<CreateCommentDto>,
+  // ): Promise<HelpYouComment> {
+  //   const existingComment = await this.entityManager.findOne(HelpYouComment, {
+  //     relations: ['user', 'user.userImage'],
+  //     where: { id },
+  //   });
+  //   for (const key in commentData) {
+  //     if (commentData.hasOwnProperty(key)) {
+  //       existingComment[key] = commentData[key];
+  //     }
+  //   }
+  //   await this.entityManager.save(HelpYouComment, existingComment);
+  //   return existingComment;
+  // }
 
   async deleteComment(comment: HelpYouComment): Promise<void> {
     await this.entityManager.remove(HelpYouComment, comment);
