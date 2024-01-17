e159190a7de9e5d45657382aa25b123e831a0e8f
NicoDora <pjh76574734@gmail.com>
2024-01-17 11:12:04 +0900

    docs(#92): dto 수정

--- src/total-count/dtos/counting.dto.ts
+++ src/total-count/dtos/counting.dto.ts
@@ -1,17 +1,29 @@
-import { IsIn } from 'class-validator';
+import { IsEnum } from 'class-validator';
 import { Action } from '../enums/action.enum';
 import { Type } from '../enums/type.enum';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CountingDto {
   mentorId?: number;
 
-  @IsIn(Object.values(Type), {
+  @ApiProperty({
+    description:
+      'countMentorBoard: 멘토 게시글 조회수, countHelpYouComment: 도와주세요 댓글 조회수, countMentorBoardLike: 멘토 게시글 좋아요 수, countBadge: 멘토 뱃지 수, countReview: 멘토 후기 수',
+    enum: Type,
+    default: Type.CountMentorBoard,
+  })
+  @IsEnum(Type, {
     message:
       'type은 countMentorBoard, countHelpYouComment, countMentorBoardLike, countBadge, countReview 중 하나만 가능합니다.',
   })
   type: Type;
 
-  @IsIn(Object.values(Action), {
+  @ApiProperty({
+    description: 'increment: 증가, decrement: 감소',
+    enum: Action,
+    default: Action.Increment,
+  })
+  @IsEnum(Action, {
     message: 'action은 increment 또는 decrement만 가능합니다.',
   })
   action: Action;
