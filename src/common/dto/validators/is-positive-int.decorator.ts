917305ab37421036f11a6f3f9bfd301d6fdf3b01
hobiJeong <jjb26433@naver.com>
2024-01-18 11:12:10 +0900

    refactor(#94): modify jsdoc

--- src/common/dto/validators/is-positive-int.decorator.ts
+++ src/common/dto/validators/is-positive-int.decorator.ts
@@ -3,7 +3,7 @@ import { Type } from 'class-transformer';
 import { IsInt, Min, ValidationOptions } from 'class-validator';
 
 /**
- * 양의 정수(0 제외)만 허용 및 number로 타입 변환
+ * 양의 정수만 허용 및 number로 타입 변환
  * Number로 타입 변환 -> 정수인지 확인 -> 1 이상인지 확인
  */
 export const IsPositiveInt = (
