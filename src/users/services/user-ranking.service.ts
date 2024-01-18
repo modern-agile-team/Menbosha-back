9b36bf06292a6736379ada3b2e65eca016370cd9
NicoDora <pjh76574734@gmail.com>
2024-01-18 14:45:23 +0900

    modify(#92): 스케줄링 자동화로 유저 랭킹 업데이트

--- src/users/services/user-ranking.service.ts
+++ src/users/services/user-ranking.service.ts
@@ -1,58 +1,68 @@
+import { Cron } from '@nestjs/schedule';
 import { UserRankingRepository } from './../repositories/user-ranking.repository';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserRankingService {
   constructor(private readonly userRankingRepository: UserRankingRepository) {}
 
+  @Cron('0 59 8 * * 1') // 매주 월요일 오전 8시 59분에 실행 (7days 칼럼 초기화 전에 실행)
   async userRanking() {
-    const allUserCounts = await this.userRankingRepository.allUserCounts();
-    const userRankingArray = allUserCounts
-      .map((user) => {
-        let totalScore =
-          user.countMentorBoard7days * 23 +
-          user.countMentorBoardLike7days * 10 +
-          user.countHelpYouComment7days * 7 +
-          user.countReview7days * 97 +
-          user.countBadge7days * 18;
+    try {
+      const allUserCounts = await this.userRankingRepository.allUserCounts();
+      const userRankingArray = allUserCounts
+        .map((user) => {
+          let totalScore =
+            user.countMentorBoard7days * 23 +
+            user.countMentorBoardLike7days * 10 +
+            user.countHelpYouComment7days * 7 +
+            user.countReview7days * 97 +
+            user.countBadge7days * 18;
 
-        if (user.countMentorBoard7days > 10) {
-          const after = user.countMentorBoard7days - 10;
-          totalScore = totalScore - after * 13;
-        }
-        if (user.countHelpYouComment7days > 20) {
-          const after = user.countHelpYouComment7days - 20;
-          totalScore = totalScore - after * 4;
-        }
-        if (user.countReview7days > 5) {
-          const after = user.countReview7days - 5;
-          totalScore = totalScore + after * 49;
-        }
-        if (totalScore < 60) {
-          return null;
-        }
-        return {
-          userId: user.userId,
-          totalScore,
-        };
-      })
-      .sort((a, b) => b.totalScore - a.totalScore)
-      .slice(0, 10);
+          if (user.countMentorBoard7days > 10) {
+            const after = user.countMentorBoard7days - 10;
+            totalScore = totalScore - after * 13;
+          }
+          if (user.countHelpYouComment7days > 20) {
+            const after = user.countHelpYouComment7days - 20;
+            totalScore = totalScore - after * 4;
+          }
+          if (user.countReview7days > 5) {
+            const after = user.countReview7days - 5;
+            totalScore = totalScore + after * 49;
+          }
+          if (totalScore < 60) {
+            return null;
+          }
+          return {
+            userId: user.userId,
+            totalScore,
+          };
+        })
+        .sort((a, b) => b.totalScore - a.totalScore)
+        .slice(0, 10);
 
-    const userIds = userRankingArray.map((user) => user.userId);
-    while (userIds.length < 10) {
-      userIds.push(null);
-    }
-
-    await this.userRankingRepository.saveUserRanking(userIds);
-
-    for (const userId of userIds) {
-      if (userId === null) {
-        break;
+      const userIds = userRankingArray.map((user) => user.userId);
+      while (userIds.length < 10) {
+        userIds.push(null);
       }
-      await this.userRankingRepository.saveUserInfo(userId);
-    }
 
-    return userRankingArray;
+      await this.userRankingRepository.saveUserRanking(userIds);
+
+      for (const userId of userIds) {
+        if (userId === null) {
+          break;
+        }
+        await this.userRankingRepository.saveUserInfo(userId);
+      }
+
+      return { message: '랭킹 업데이트 성공' };
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(
+        '랭킹 업데이트 중 에러가 발생했습니다.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
