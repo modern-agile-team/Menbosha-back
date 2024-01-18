0399a6f05e7933d0ae47446851f4852d1738e3b7
hobiJeong <jjb26433@naver.com>
2024-01-18 13:29:25 +0900

    modify(#112): modify folder structure

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -14,7 +14,7 @@ import { MentorBoard } from 'src/boards/entities/mentor-board.entity';
 import { HelpMeBoard } from 'src/boards/entities/help-me-board.entity';
 import { UserReview } from './user-review.entity';
 import { UserBadge } from './user-badge.entity';
-import { CategoryList } from '../../common/entity/category-list.entity';
+import { CategoryList } from '../../category/entity/category-list.entity';
 import { UserIntro } from './user-intro.entity';
 import { TotalCount } from 'src/total-count/entities/total-count.entity';
 import { MentorBoardLike } from 'src/boards/entities/mentor-board-like.entity';
