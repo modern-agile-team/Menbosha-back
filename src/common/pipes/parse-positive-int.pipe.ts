89ae1a9d38e41c2ec964a20c1e26d1eee6ee8452
hobiJeong <jjb26433@naver.com>
2024-01-18 16:34:48 +0900

    feat(#112): create jsdoc

--- src/common/pipes/parse-positive-int.pipe.ts
+++ src/common/pipes/parse-positive-int.pipe.ts
@@ -4,6 +4,10 @@ import {
   PipeTransform,
 } from '@nestjs/common';
 
+/**
+ * 양의 정수인지 확인하고 맞다면 Number 타입으로 변환
+ * 아니라면 에러
+ */
 export class ParsePositiveIntPipe implements PipeTransform<string> {
   transform(value: string, metadata: ArgumentMetadata): number {
     const { type, data } = metadata;
@@ -14,8 +18,17 @@ export class ParsePositiveIntPipe implements PipeTransform<string> {
       );
     }
 
+    /**
+     * 10진수 정수형으로 변환 후 return
+     */
     return parseInt(value, 10);
   }
+  /**
+   * 1. string || number type인지 확인
+   * 2. 정수 허용(소수 제외)
+   * 3. Infinite, NaN 거기기
+   * 4. 양의 정수인지 확인
+   */
   private isPositiveNumeric(value: string): boolean {
     return (
       ['string', 'number'].includes(typeof value) &&
