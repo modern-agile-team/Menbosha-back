0a51b0d5104b432383d684f320434a8ab126d34f
hobiJeong <jjb26433@naver.com>
2024-01-20 19:59:40 +0900

    feat(#127): define dto property

--- src/mentors/dtos/mentor-review-checklist.dto.ts
+++ src/mentors/dtos/mentor-review-checklist.dto.ts
@@ -1,3 +1,17 @@
 import { MentorReviewChecklist } from 'src/users/entities/mentor-review-checklist.entity';
 
-export class MentorReviewChecklistDto implements MentorReviewChecklist {}
+export class MentorReviewChecklistDto
+  implements Omit<MentorReviewChecklist, 'mentorReview'>
+{
+  id: number;
+  mentorReviewId: number;
+  isAccurate: boolean;
+  isBad: boolean;
+  isClear: boolean;
+  isFun: boolean;
+  isGoodWork: boolean;
+  isInformative: boolean;
+  isKindness: boolean;
+  isQuick: boolean;
+  isStuffy: boolean;
+}
--- src/mentors/dtos/mentor-review.dto.ts
+++ src/mentors/dtos/mentor-review.dto.ts
@@ -1,3 +1,13 @@
+import { MentorReviewChecklist } from 'src/users/entities/mentor-review-checklist.entity';
 import { MentorReview } from 'src/users/entities/mentor-review.entity';
 
-export class MentorReviewDto implements MentorReview {}
+export class MentorReviewDto
+  implements Omit<MentorReview, 'mentor' | 'mentee'>
+{
+  id: number;
+  mentorId: number;
+  menteeId: number;
+  review: string;
+  mentorReviewChecklists: MentorReviewChecklist[];
+  createdAt: Date;
+}
