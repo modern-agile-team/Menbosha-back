ec037aa6d091b164d6e76ec1629037db1d42e16c
hobiJeong <jjb26433@naver.com>
2024-01-23 22:26:10 +0900

    doc(#132): modify swagger api docs

--- src/boards/swagger-decorators/mentorBoard/find-all-hot-posts-with-pagination.decorator.ts
+++ src/boards/swagger-decorators/mentorBoard/find-all-hot-posts-with-pagination.decorator.ts
@@ -26,6 +26,34 @@ export function ApiFindAllHotPostsWithPagination() {
         },
       },
     }),
+    ApiResponse({
+      status: 400,
+      description: '400 error',
+      content: {
+        JSON: {
+          examples: {
+            'validation failed': {
+              value: {
+                message: [
+                  'categoryId must be an integer number',
+                  'categoryId must not be less than 1',
+                  'orderField must be one of the following values: id, userId, head, body, createdAt, updatedAt, categoryId, popularAt',
+                  'sortOrder must be one of the following values: DESC, ASC',
+                  'page must be an integer number',
+                  'page must not be less than 1',
+                  'pageSize must not be less than 5',
+                  'pageSize must be an integer number',
+                  'property [허용하지 않은 데이터] should not exist',
+                ],
+                error: 'Bad Request',
+                statusCode: 400,
+              },
+              description: '유효성 검사 실패',
+            },
+          },
+        },
+      },
+    }),
     ApiExtraModels(ResponseMentorBoardHotPostPaginationDto),
   );
 }
