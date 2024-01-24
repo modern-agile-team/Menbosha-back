1327f2f43054410deb1ef1548a117ed394886166
NicoDora <pjh76574734@gmail.com>
2024-01-24 16:47:33 +0900

    modify(#138): 토큰 가드 예외처리

--- src/config/guards/jwt-optional.guard.ts
+++ src/config/guards/jwt-optional.guard.ts
@@ -7,15 +7,21 @@ export class JwtOptionalGuard {
 
   async canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
-    const accessToken = request.headers['access_token'];
-    if (!accessToken) {
+    const authorization = request.headers['authorization'];
+    if (!authorization) {
+      const userId = null;
+      request.user = { userId };
+      return true;
+    }
+
+    const [type, accessToken] = authorization.split(' ');
+    if (type !== 'Bearer') {
       const userId = null;
       request.user = { userId };
       return true;
     }
 
     const userId = await this.tokenService.decodeToken(accessToken);
-
     request.user = { userId };
 
     return true;
