86db99f6cf924fd9f5794b247cedda0f9ad1c098
hobiJeong <jjb26433@naver.com>
2024-01-30 21:26:58 +0900

    modify(#169): modify mentor board page query dto

--- src/boards/dto/mentorBoard/mentor-board-page-query.dto.ts
+++ src/boards/dto/mentorBoard/mentor-board-page-query.dto.ts
@@ -1,13 +1,14 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 
-import { IsEnum, IsOptional } from 'class-validator';
+import { IsBooleanString, IsEnum, IsOptional } from 'class-validator';
 import { MentorBoardOrderField } from 'src/boards/constants/mentor-board-order-field.enum';
 import { SortOrder } from '../../../common/constants/sort-order.enum';
 import { PageQueryDto } from 'src/common/dto/page-query.dto';
 import { IsPositiveInt } from 'src/common/decorators/validators/is-positive-int.decorator';
+import { Type } from 'class-transformer';
 
 export class MentorBoardPageQueryDto extends PageQueryDto {
-  @ApiPropertyOptional({
+  @ApiProperty({
     description: '조회할 category의 id',
     format: 'integer',
     default: 1,
@@ -17,7 +18,16 @@ export class MentorBoardPageQueryDto extends PageQueryDto {
   @IsPositiveInt()
   categoryId: number = 1;
 
-  @ApiPropertyOptional({
+  @ApiProperty({
+    description: '인기 게시글만 불러올 지, 인기 멘토 게시판을 불러올 지 여부',
+    default: false,
+  })
+  @IsOptional()
+  @IsBooleanString()
+  @Type(() => Boolean)
+  loadOnlyPopular: boolean = false;
+
+  @ApiProperty({
     description: '정렬의 기준으로 잡을 필드',
     enum: MentorBoardOrderField,
     default: MentorBoardOrderField.id,
@@ -26,7 +36,7 @@ export class MentorBoardPageQueryDto extends PageQueryDto {
   @IsEnum(MentorBoardOrderField)
   orderField: MentorBoardOrderField = MentorBoardOrderField.id;
 
-  @ApiPropertyOptional({
+  @ApiProperty({
     description: '오름차순 혹은 내림차순',
     enum: SortOrder,
     default: SortOrder.Asc,
