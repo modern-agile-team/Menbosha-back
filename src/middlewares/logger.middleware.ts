795ecc1bec7b89875a7c194825f3a33a869386d8
hobiJeong <jjb26433@naver.com>
2024-01-30 18:13:45 +0900

    setting(#167): setting logger

---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,33 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger('HTTP');
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    const { ip, method, originalUrl } = request;
+    const userAgent = request.get('user-agent') || '';
+    const startTime = new Date().getTime();
+
+    response.on('finish', () => {
+      console.log(response);
+      const { statusCode } = response;
+      const contentLength = response.get('content-length');
+      const endTime = new Date().getTime();
+      const duration = `${endTime - startTime}ms`;
+
+      this.logger.log({
+        duration,
+        method,
+        originalUrl,
+        statusCode,
+        contentLength,
+        userAgent,
+        ip,
+      });
+    });
+
+    next();
+  }
+}
