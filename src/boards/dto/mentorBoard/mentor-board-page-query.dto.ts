8b094f9dc21b93de67017cbff93046f69db7e96a
hobiJeong <jjb26433@naver.com>
2024-01-31 00:31:56 +0900

    delete(#169): delete unused import

--- src/boards/dto/mentorBoard/mentor-board-page-query.dto.ts
+++ src/boards/dto/mentorBoard/mentor-board-page-query.dto.ts
@@ -1,17 +1,10 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
-import {
-  IsBoolean,
-  IsBooleanString,
-  IsEnum,
-  IsOptional,
-  IsString,
-} from 'class-validator';
+import { IsBoolean, IsEnum, IsOptional, IsString } from 'class-validator';
 import { MentorBoardOrderField } from 'src/boards/constants/mentor-board-order-field.enum';
 import { SortOrder } from '../../../common/constants/sort-order.enum';
 import { PageQueryDto } from 'src/common/dto/page-query.dto';
 import { IsPositiveInt } from 'src/common/decorators/validators/is-positive-int.decorator';
-import { Type } from 'class-transformer';
 import { ParseOptionalBoolean } from 'src/common/transformers/parse-optional-boolean.transformer';
 
 export class MentorBoardPageQueryDto extends PageQueryDto {
