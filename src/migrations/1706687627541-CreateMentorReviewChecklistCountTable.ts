5c226889890d4ded751a753ee93914fa46cd369e
hobiJeong <jjb26433@naver.com>
2024-01-31 16:58:22 +0900

    feat(#172): create util file

---
+++ src/migrations/1706687627541-CreateMentorReviewChecklistCountTable.ts
@@ -0,0 +1,9 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateMentorReviewChecklistCountTable1706687627541
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
---
+++ src/migrations/__utils/util.ts
@@ -0,0 +1,67 @@
+import { TableColumnOptions } from 'typeorm';
+
+export const generatePrimaryColumn = (
+  comment: string = '고유 ID',
+): TableColumnOptions => {
+  return {
+    name: 'id',
+    type: 'int',
+    unsigned: true,
+    isPrimary: true,
+    isNullable: false,
+    isGenerated: true,
+    generationStrategy: 'increment',
+    comment,
+  };
+};
+
+export const generateBooleanColumn = (
+  name: string,
+  comment: string,
+): TableColumnOptions => {
+  return {
+    name: name,
+    type: 'tinyint',
+    length: '1',
+    unsigned: true,
+    default: 0,
+    isNullable: false,
+    comment: comment,
+  };
+};
+
+export const generateCreatedAtColumn = (
+  comment: string = '생성 일자',
+): TableColumnOptions => {
+  return {
+    name: 'created_at',
+    type: 'timestamp',
+    isNullable: false,
+    default: 'CURRENT_TIMESTAMP',
+    comment,
+  };
+};
+
+export const generateUpdatedAtColumn = (
+  comment: string = '수정 일자',
+): TableColumnOptions => {
+  return {
+    name: 'updated_at',
+    type: 'timestamp',
+    isNullable: false,
+    default: 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+    comment,
+  };
+};
+
+export const generateDeletedAtColumn = (
+  comment: string = '삭제 일자',
+): TableColumnOptions => {
+  return {
+    name: 'deleted_at',
+    type: 'timestamp',
+    isNullable: true,
+    comment,
+  };
+};
