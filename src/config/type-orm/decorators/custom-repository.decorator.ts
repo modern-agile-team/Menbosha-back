c86f3c52553d813fa5ea6281b7560d17f71f5b69
hobiJeong <jjb26433@naver.com>
2024-01-31 18:45:17 +0900

    delete(#175): delete custom repository

--- src/config/type-orm/decorators/custom-repository.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { TYPEORM_EX_CUSTOM_REPOSITORY } from '../constants/type-orm.token';
-
-export function CustomRepository(entity: any): ClassDecorator {
-  return SetMetadata(TYPEORM_EX_CUSTOM_REPOSITORY, entity);
-}
