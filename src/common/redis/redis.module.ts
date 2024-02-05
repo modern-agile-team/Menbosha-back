3bc8e6a44487d7883a120a2b22ef3a5d32d75cd2
NicoDora <pjh76574734@gmail.com>
2024-02-05 16:25:03 +0900

    deploy(#194): 안쓰는 파일 삭제

--- src/common/redis/redis.module.ts
+++ src/common/redis/redis.module.ts
@@ -2,7 +2,6 @@ import { RedisService } from './redis.service';
 import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import * as redisStore from 'cache-manager-ioredis';
-import { RedisOptions } from 'src/config/redis-options.constants';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
