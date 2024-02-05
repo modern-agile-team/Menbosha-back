f372df6a10c4d2dd385a53ad02aeeebe5b84fa38
NicoDora <pjh76574734@gmail.com>
2024-02-05 15:38:38 +0900

    deploy(#30): redis module 수정

--- src/common/redis/redis.module.ts
+++ src/common/redis/redis.module.ts
@@ -1,12 +1,18 @@
 import { RedisService } from './redis.service';
 import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
+import * as redisStore from 'cache-manager-ioredis';
 import { RedisOptions } from 'src/config/redis-options.constants';
 
 @Module({
   imports: [
-    CacheModule.register({ isGlobal: true, RedisOptions }),
-    // CacheModule.registerAsync(RedisOptions),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASSWORD,
+    }),
   ],
   controllers: [],
   providers: [RedisService],
