29806ed5774c1c5e7c06bb6fb5633fe16eb88f9e
hobiJeong <jjb26433@naver.com>
2024-02-05 23:08:27 +0900

    refactor(#189): set response type

--- src/mentors/controllers/mentors.controller.ts
+++ src/mentors/controllers/mentors.controller.ts
@@ -7,6 +7,7 @@ import {
 } from '@nestjs/common';
 import { MentorListPageQueryDto } from 'src/users/dtos/mentor-list-page-query.dto';
 import { MentorsService } from '../services/mentors.service';
+import { MentorPaginationResponseDto } from '../dtos/mentors-pagination-response.dto';
 
 @UsePipes(
   new ValidationPipe({
@@ -22,7 +23,7 @@ export class MentorsController {
   @Get()
   findAllMentorsAndCount(
     @Query() mentorListPageQueryDto: MentorListPageQueryDto,
-  ) {
+  ): Promise<MentorPaginationResponseDto> {
     return this.mentorsService.findAllMentorsAndCount(mentorListPageQueryDto);
   }
 }
