776f2f24f41d1f9e6cbb1b1016a9491e4600aa26
hobiJeong <jjb26433@naver.com>
2024-02-05 14:33:57 +0900

    feat(#189): create dto validator

--- src/users/dtos/mentor-list-page-query.dto.ts
+++ src/users/dtos/mentor-list-page-query.dto.ts
@@ -1,6 +1,9 @@
 import { PageQueryDto } from 'src/common/dto/page-query.dto';
 import { User } from '../entities/user.entity';
 import { SortOrder } from 'src/common/constants/sort-order.enum';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { IsPositiveInt } from 'src/common/decorators/validators/is-positive-int.decorator';
 
 /**
  * @todo create provider enum, mentorOrderField
@@ -9,19 +12,38 @@ export class MentorListPageQueryDto
   extends PageQueryDto
   implements Partial<Pick<User, 'id'>>
 {
+  @ApiPropertyOptional({
+    description: '멘토 고유 ID 필터링',
+    format: 'integer',
+  })
+  @IsString()
+  @IsPositiveInt()
   id?: number;
 
-  provider?: string;
-
+  @ApiPropertyOptional({
+    description: '멘토 이름 필터링',
+  })
+  @IsOptional()
+  @IsString()
   name?: string;
 
-  loadOnlyMentor: boolean = true;
+  @ApiPropertyOptional({
+    description: '멘토 활동 카테고리 고유 ID',
+    format: 'integer',
+    default: 1,
+  })
+  @IsOptional()
+  @IsPositiveInt()
+  activityCategoryId: number = 1;
 
-  hopeCategoryId: number;
-
-  activityCategoryId: number;
-
+  @IsOptional()
   orderField;
 
+  @ApiPropertyOptional({
+    description: '오름차순 혹은 내림차순',
+    enum: SortOrder,
+  })
+  @IsOptional()
+  @IsEnum(SortOrder)
   sortOrder: SortOrder = SortOrder.Asc;
 }
