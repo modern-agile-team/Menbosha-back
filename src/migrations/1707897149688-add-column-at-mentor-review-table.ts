95371b44bdf40d84ba28ca1724d03e90c13595cb
hobiJeong <jjb26433@naver.com>
2024-02-14 16:56:51 +0900

    modify(#224): modify migration file

--- src/migrations/1707897149688-add-column-at-mentor-review-table.ts
+++ src/migrations/1707897149688-add-column-at-mentor-review-table.ts
@@ -1,5 +1,5 @@
 import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
-import { generateBooleanColumn } from './__utils/util';
+import { generateBooleanColumn, generateCountColumn } from './__utils/util';
 
 export class AddColumnAtMentorReviewTable1707897149688
   implements MigrationInterface
@@ -11,9 +11,20 @@ export class AddColumnAtMentorReviewTable1707897149688
         generateBooleanColumn('is_understand_well', '이해가 잘돼요'),
       ),
     );
+
+    await queryRunner.addColumn(
+      'mentor_review_checklist_count',
+      new TableColumn(
+        generateCountColumn('is_understand_well', '이해가 잘돼요'),
+      ),
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropColumn('mentor_review', 'is_understand_well');
+    await queryRunner.dropColumn(
+      'mentor_review_checklist_count',
+      'is_understand_well',
+    );
   }
 }
