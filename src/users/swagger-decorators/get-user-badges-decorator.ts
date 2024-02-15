95f8c45c8de7204b55be5c57c94fbe9238eeaa5d
2swo <swlee456@naver.com>
2024-02-15 16:14:35 +0900

    doc(#214) swagger문서 최적화

--- src/users/swagger-decorators/get-user-badges-decorator.ts
+++ src/users/swagger-decorators/get-user-badges-decorator.ts
@@ -9,7 +9,7 @@ export function ApiPostUserBadges() {
     }),
     ApiResponse({
       status: 201,
-      description: '성공적으로 뱃지를 가져올 경우',
+      description: '새로운 뱃지가 추가되고, 기존의 뱃지도 남아있는 경우',
       content: {
         JSON: {
           example: {
@@ -41,6 +41,64 @@ export function ApiPostUserBadges() {
                     badgeId: 'number',
                     id: 'number',
                     createdAt: '2024-02-14T21:59:28.000Z',
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      },
+    }),
+    ApiResponse({
+      status: 201,
+      description: '새로운 뱃지는 없고, 기존의 뱃지만 불러올 경우',
+      content: {
+        JSON: {
+          example: {
+            data: [
+              [
+                {
+                  id: 'number',
+                  userId: 'number',
+                  badgeId: 'number',
+                  createdAt: '2024-02-14T21:58:03.000Z',
+                },
+                {
+                  id: 'number',
+                  userId: 'number',
+                  badgeId: 'number',
+                  createdAt: '2024-02-14T21:58:03.000Z',
+                },
+              ],
+              {
+                acquiredBadges: [],
+              },
+            ],
+          },
+        },
+      },
+    }),
+    ApiResponse({
+      status: 201,
+      description: '기존의 뱃지는 없고, 새로운 뱃지만 추가 될 경우',
+      content: {
+        JSON: {
+          example: {
+            data: [
+              [],
+              {
+                acquiredBadges: [
+                  {
+                    id: 'number',
+                    userId: 'number',
+                    badgeId: 'number',
+                    createdAt: '2024-02-14T21:58:03.000Z',
+                  },
+                  {
+                    id: 'number',
+                    userId: 'number',
+                    badgeId: 'number',
+                    createdAt: '2024-02-14T21:58:03.000Z',
                   },
                 ],
               },
