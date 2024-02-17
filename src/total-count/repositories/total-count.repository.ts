913c7409958ad5a7be0d14727f421a4c25263f12
NicoDora <pjh76574734@gmail.com>
2024-02-17 11:20:24 +0900

    🔥 delete(#213): 안쓰는 카운팅 관련 코드 삭제

--- src/total-count/repositories/total-count.repository.ts
+++ src/total-count/repositories/total-count.repository.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { EntityManager } from 'typeorm';
 import { TotalCount } from '../entities/total-count.entity';
-import { Type } from '../enums/type.enum';
-import { Action } from '../enums/action.enum';
 
 @Injectable()
 export class TotalCountRepository {
@@ -19,31 +17,6 @@ export class TotalCountRepository {
     await entityManager.insert('mentor_review_checklist_count', {
       userId,
     });
-  }
-
-  async counting(
-    entityManager: EntityManager,
-    userId: number,
-    type: Type,
-    action: Action,
-  ) {
-    if (action === Action.Increment) {
-      await entityManager.increment(TotalCount, { userId }, type, 1);
-      await entityManager.increment(
-        TotalCount,
-        { userId },
-        `${type}InSevenDays`,
-        1,
-      );
-    } else {
-      await entityManager.decrement(TotalCount, { userId }, type, 1);
-      await entityManager.decrement(
-        TotalCount,
-        { userId },
-        `${type}InSevenDays`,
-        1,
-      );
-    }
   }
 
   async clear7DaysCount() {
--- src/total-count/services/total-count.service.ts
+++ src/total-count/services/total-count.service.ts
@@ -1,7 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { TotalCountRepository } from '../repositories/total-count.repository';
-import { Type } from '../enums/type.enum';
-import { Action } from '../enums/action.enum';
 import { Cron } from '@nestjs/schedule';
 import { EntityManager } from 'typeorm';
 
@@ -21,50 +19,6 @@ export class TotalCountService {
       entityManager,
       userId,
     );
-  }
-
-  // TODO : badgeCount subscriber로 구현 (뱃지가 완성되면 작업 시작)
-  // TODO : 위에 subscriber들이 다 구현되면 count 관련 코드 삭제하기
-  async counting(
-    entityManager: EntityManager,
-    userId: number,
-    type: Type,
-    action: Action,
-    mentorId?: number,
-  ) {
-    try {
-      if (type === Type.MentorBoardLikeCount || type === Type.ReviewCount) {
-        if (!mentorId) {
-          throw new HttpException(
-            'mentorId가 필요한 요청입니다.',
-            HttpStatus.BAD_REQUEST,
-          );
-        }
-        await this.totalCountRepository.counting(
-          entityManager,
-          mentorId,
-          type,
-          action,
-        );
-      } else {
-        await this.totalCountRepository.counting(
-          entityManager,
-          userId,
-          type,
-          action,
-        );
-      }
-      return { message: '카운팅 성공' };
-    } catch (error) {
-      if (error instanceof HttpException) throw error;
-      else {
-        console.log(error);
-        throw new HttpException(
-          '카운팅 도중 에러가 발생했습니다.',
-          HttpStatus.INTERNAL_SERVER_ERROR,
-        );
-      }
-    }
   }
 
   @Cron('0 0 9 * * 1') // 매주 월요일 오전 9시에 실행
