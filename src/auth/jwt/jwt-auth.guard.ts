e4c41d83beddc494e716093555a76be64c598f4e
NicoDora <pjh76574734@gmail.com>
2024-02-17 15:55:18 +0900

    ♻️ refactor(#233): JWT 토큰 가드 각각의 클래스로 분리해서 처리

--- src/auth/jwt/jwt-auth.guard.ts
+++ src/auth/jwt/jwt-auth.guard.ts
@@ -5,68 +5,81 @@ import {
   Injectable,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { request } from 'express';
 import { Observable } from 'rxjs';
 
-function AuthGuardMixin(strategy: string) {
-  abstract class MixinAuthGuard extends AuthGuard(strategy) {
-    canActivate(
-      context: ExecutionContext,
-    ): boolean | Promise<boolean> | Observable<boolean> {
-      const request = context.switchToHttp().getRequest();
-      const authorization = request.headers['authorization'];
-      if (!authorization) {
-        throw new HttpException('jwt must be provided', HttpStatus.BAD_REQUEST);
-      }
-      return super.canActivate(context);
+@Injectable()
+export class AccessTokenAuthGuard extends AuthGuard('accessToken') {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authorization = request.headers['authorization'];
+    if (!authorization) {
+      throw new HttpException('jwt must be provided', HttpStatus.BAD_REQUEST);
     }
+    return super.canActivate(context);
+  }
 
-    handleRequest(
-      err: any,
-      user: any,
-      info: { message: string | Record<string, any> },
-      context: ExecutionContext,
-    ) {
-      try {
-        if (user) {
-          return super.handleRequest(err, user, info, context);
-        }
-        
-        if (err instanceof HttpException) throw err;
-       
-        throw new HttpException(info.message, this.getStatus(info.message));
-      } catch (error) {
-        if (error instanceof HttpException) throw error;
-        else {
-          console.log(error.message);
-          throw new HttpException('jwt error', HttpStatus.BAD_REQUEST);
-        }
+  handleRequest(
+    err: any,
+    user: any,
+    info: { message: string | Record<string, any> },
+    context: ExecutionContext,
+  ) {
+    try {
+      if (user) {
+        return super.handleRequest(err, user, info, context);
       }
-    }
 
-    getStatus(message: string | Record<string, any>): HttpStatus {
-      switch (message) {
-        case 'jwt expired':
-        case 'invalid signature':
-          return HttpStatus.UNAUTHORIZED;
-        case 'jwt must be provided':
-        case 'jwt malformed':
-        case 'invalid token':
-          return HttpStatus.BAD_REQUEST;
-        default:
-          return HttpStatus.BAD_REQUEST;
+      if (err instanceof HttpException) throw err;
+
+      throw new HttpException(info.message, getStatus(info.message));
+    } catch (error) {
+      if (error instanceof HttpException) throw error;
+      else {
+        console.log(error.message);
+        throw new HttpException('jwt error', HttpStatus.BAD_REQUEST);
       }
     }
   }
-
-  return MixinAuthGuard;
 }
 
 @Injectable()
-export class AccessTokenAuthGuard extends AuthGuardMixin('accessToken') {}
+export class RefreshTokenAuthGuard extends AuthGuard('refreshToken') {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authorization = request.headers['authorization'];
+    if (!authorization) {
+      throw new HttpException('jwt must be provided', HttpStatus.BAD_REQUEST);
+    }
+    return super.canActivate(context);
+  }
 
-@Injectable()
-export class RefreshTokenAuthGuard extends AuthGuardMixin('refreshToken') {}
+  handleRequest(
+    err: any,
+    user: any,
+    info: { message: string | Record<string, any> },
+    context: ExecutionContext,
+  ) {
+    try {
+      if (user) {
+        return super.handleRequest(err, user, info, context);
+      }
+
+      if (err instanceof HttpException) throw err;
+
+      throw new HttpException(info.message, getStatus(info.message));
+    } catch (error) {
+      if (error instanceof HttpException) throw error;
+      else {
+        console.log(error.message);
+        throw new HttpException('jwt error', HttpStatus.BAD_REQUEST);
+      }
+    }
+  }
+}
 
 @Injectable()
 export class AccessTokenOptionalAuthGuard extends AuthGuard('accessToken') {
@@ -77,5 +90,19 @@ export class AccessTokenOptionalAuthGuard extends AuthGuard('accessToken') {
     context: ExecutionContext,
   ): any {
     return user;
+  }
+}
+
+function getStatus(message: string | Record<string, any>): HttpStatus {
+  switch (message) {
+    case 'jwt expired':
+    case 'invalid signature':
+      return HttpStatus.UNAUTHORIZED;
+    case 'jwt must be provided':
+    case 'jwt malformed':
+    case 'invalid token':
+      return HttpStatus.BAD_REQUEST;
+    default:
+      return HttpStatus.BAD_REQUEST;
   }
 }
