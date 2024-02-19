4be207a791249d6e76181370e86130e82a682635
NicoDora <pjh76574734@gmail.com>
2024-02-19 15:38:12 +0900

    ✏️ modify(#239): jwt.strategy.ts에서 redis에 저장된 토큰과 일치하는지 확인하는 로직 추가

--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -1,23 +1,37 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { RedisService } from 'src/common/redis/redis.service';
 
 @Injectable()
 export class AccessTokenStrategy extends PassportStrategy(
   Strategy,
   'accessToken',
 ) {
-  constructor() {
+  constructor(private readonly redisService: RedisService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET_KEY,
+      passReqToCallback: true,
     });
   }
 
-  async validate(payload: any) {
+  async validate(request: any, payload: any) {
     if (payload.sub !== 'accessToken') {
       throw new HttpException('invalid token type', HttpStatus.BAD_REQUEST);
+    }
+
+    const tokenFromRequest = request.headers.authorization.split(' ')[1];
+    const tokenInRedis = await this.redisService.getToken(
+      `${payload.userId}-accessToken`,
+    );
+
+    if (!tokenInRedis) {
+      throw new HttpException('token not found', HttpStatus.NOT_FOUND);
+    } else if (tokenInRedis !== tokenFromRequest) {
+      throw new HttpException('token mismatch', HttpStatus.UNAUTHORIZED);
     }
     return { id: payload.userId };
   }
@@ -28,17 +42,29 @@ export class RefreshTokenStrategy extends PassportStrategy(
   Strategy,
   'refreshToken',
 ) {
-  constructor() {
+  constructor(private readonly redisService: RedisService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET_KEY,
+      passReqToCallback: true,
     });
   }
 
-  async validate(payload: any) {
+  async validate(request: any, payload: any) {
     if (payload.sub !== 'refreshToken') {
       throw new HttpException('invalid token type', HttpStatus.BAD_REQUEST);
+    }
+
+    const tokenFromRequest = request.headers.authorization.split(' ')[1];
+    const tokenInRedis = await this.redisService.getToken(
+      `${payload.userId}-refreshToken`,
+    );
+
+    if (!tokenInRedis) {
+      throw new HttpException('token not found', HttpStatus.NOT_FOUND);
+    } else if (tokenInRedis !== tokenFromRequest) {
+      throw new HttpException('token mismatch', HttpStatus.UNAUTHORIZED);
     }
     return { id: payload.userId };
   }
