7104c124ae6f53c76930e394272096f5123588fc
hobiJeong <jjb26433@naver.com>
2024-02-21 16:56:00 +0900

    feat(#251): create dto

---
+++ src/reports/dto/report.dto.ts
@@ -0,0 +1,50 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Exclude } from 'class-transformer';
+import { UserReportType } from 'src/reports/constants/report-type.enum';
+import { REPORT_REASON_LENGTH } from 'src/reports/constants/report.constant';
+import { Report } from 'src/reports/entities/report.entity';
+
+export class ReportDto implements Omit<Report, 'reportUser' | 'reportedUser'> {
+  @ApiProperty({
+    description: '신고 고유 ID',
+    format: 'integer',
+    minimum: 1,
+  })
+  id: number;
+
+  @ApiProperty({
+    description: '신고 고유 ID',
+    format: 'integer',
+    minimum: 1,
+  })
+  reportUserId: number;
+
+  @ApiProperty({
+    description: '신고 고유 ID',
+    format: 'integer',
+    minimum: 1,
+  })
+  reportedUserId: number;
+
+  @ApiProperty({
+    description: '신고 사유',
+    minLength: REPORT_REASON_LENGTH.MIN,
+    maxLength: REPORT_REASON_LENGTH.MAX,
+  })
+  reason: string;
+
+  @ApiProperty({
+    description: '신고 타입',
+    enum: UserReportType,
+  })
+  type: UserReportType;
+
+  @ApiProperty({
+    description: '생성 일자',
+    format: 'timestamp',
+  })
+  createdAt: Date;
+
+  @Exclude()
+  deletedAt: Date | null;
+}
