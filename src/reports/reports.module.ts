74d1492189ca07c9e0495915c72e2135f01b3f2e
hobiJeong <jjb26433@naver.com>
2024-02-22 13:25:26 +0900

    feat(#251): imports user module and provider

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReportsController } from './controllers/reports.controller';
 import { ReportsService } from './services/reports.service';
+import { UserModule } from 'src/users/user.module';
+import { ReportRepository } from 'src/reports/repositories/report.repository';
 
 @Module({
+  imports: [UserModule],
   controllers: [ReportsController],
-  providers: [ReportsService],
+  providers: [ReportsService, ReportRepository],
 })
 export class ReportsModule {}
