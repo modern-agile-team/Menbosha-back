4d1591c569e2d55bf8637e78a9c41de87918ea25
hobiJeong <jjb26433@naver.com>
2024-02-22 13:25:38 +0900

    feat(#251): create post api

--- src/reports/controllers/reports.controller.ts
+++ src/reports/controllers/reports.controller.ts
@@ -1,7 +1,44 @@
-import { Controller, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Param,
+  Post,
+  UseGuards,
+  UseInterceptors,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AccessTokenAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
+import { GetUserId } from 'src/common/decorators/get-userId.decorator';
+import { SuccessResponseInterceptor } from 'src/common/interceptors/success-response.interceptor';
+import { ParsePositiveIntPipe } from 'src/common/pipes/parse-positive-int.pipe';
+import { CreateReportBodyDto } from 'src/reports/dto/create-report-body.dto';
+import { ReportsService } from 'src/reports/services/reports.service';
+import { ApiCreateReportDecorator } from 'src/reports/swagger-decorators/create-report.decorator';
 
+@UsePipes(
+  new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }),
+)
+@UseInterceptors(ClassSerializerInterceptor, SuccessResponseInterceptor)
+@ApiTags('report')
 @Controller('users/:userId/reports')
 export class ReportsController {
+  constructor(private readonly reportsService: ReportsService) {}
+
   @Post()
-  create(@Param('userId') userId: number) {}
+  @UseGuards(AccessTokenAuthGuard)
+  @ApiCreateReportDecorator()
+  create(
+    @Body() createReportBodyDto: CreateReportBodyDto,
+    @Param('userId', ParsePositiveIntPipe) userId: number,
+    @GetUserId() myId: number,
+  ) {
+    return this.reportsService.create(createReportBodyDto, myId, userId);
+  }
 }
