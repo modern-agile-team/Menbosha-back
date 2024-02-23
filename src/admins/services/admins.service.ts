6aaee24d7d18f376c23dc88ae89a0720ddd124d1
hobiJeong <jjb26433@naver.com>
2024-02-24 01:46:50 +0900

    feat(#251): create put update service for admin api

--- src/admins/services/admins.service.ts
+++ src/admins/services/admins.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PutUpdateUserForAdminDto } from 'src/admins/dtos/put-update-user-for-admin.dto';
+import { UserResponseForAdminDto } from 'src/admins/dtos/user-response-for-admin.dto';
+import { ADMIN_ERROR_CODE } from 'src/constants/error/admin/admin-error-code.constant';
+import { AdminException } from 'src/http-exceptions/exceptions/admin-exception';
+import { UserService } from 'src/users/services/user.service';
 
 @Injectable()
-export class AdminsService {}
+export class AdminsService {
+  constructor(private readonly userService: UserService) {}
+
+  async putUpdateUserStatus(
+    adminId: number,
+    userId: number,
+    putUpdateUserForAdminDto: PutUpdateUserForAdminDto,
+  ) {
+    const existUser = await this.userService.findOneByOrNotFound({
+      where: { id: userId },
+    });
+
+    if (adminId !== userId && existUser.admin) {
+      throw new AdminException({ code: ADMIN_ERROR_CODE.DENIED_FOR_ADMINS });
+    }
+
+    const updateResult = await this.userService.updateUser(userId, {
+      ...putUpdateUserForAdminDto,
+    });
+
+    if (!updateResult.affected) {
+      throw new InternalServerErrorException('유저 업데이트 중 서버 에러 발생');
+    }
+
+    return new UserResponseForAdminDto({
+      ...existUser,
+      ...putUpdateUserForAdminDto,
+    });
+  }
+}
