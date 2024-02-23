1fddca09f2da626035c2a92b4e653d52d36a4c58
hobiJeong <jjb26433@naver.com>
2024-02-23 22:38:05 +0900

    feat(#251): apply subscriber

---
+++ src/subscribers/banned-user.subscriber.ts
@@ -0,0 +1,27 @@
+import { BannedUser } from 'src/admins/banned-user/entities/banned-user.entity';
+import { UserStatus } from 'src/users/constants/user-status.enum';
+import { User } from 'src/users/entities/user.entity';
+import {
+  EntitySubscriberInterface,
+  EventSubscriber,
+  InsertEvent,
+} from 'typeorm';
+
+@EventSubscriber()
+export class BannedUserSubscriber
+  implements EntitySubscriberInterface<BannedUser>
+{
+  listenTo() {
+    return BannedUser;
+  }
+
+  async afterInsert(event: InsertEvent<BannedUser>): Promise<void> {
+    const bannedUser = event.entity;
+
+    console.log(event.manager);
+
+    await event.manager
+      .getRepository(User)
+      .update({ id: bannedUser.bannedUserId }, { status: UserStatus.INACTIVE });
+  }
+}
