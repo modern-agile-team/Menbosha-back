33c83cb801d422a4ce70ba4424ce9cbed0020c80
hobiJeong <jjb26433@naver.com>
2024-02-23 15:37:48 +0900

    feat(#251): imports banned user module

--- src/admins/admins.module.ts
+++ src/admins/admins.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AdminsController } from './controllers/admins.controller';
 import { AdminsService } from './services/admins.service';
 import { UserModule } from 'src/users/user.module';
+import { BannedUserModule } from 'src/admins/banned-user/banned-users.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, BannedUserModule],
   controllers: [AdminsController],
   providers: [AdminsService],
 })
