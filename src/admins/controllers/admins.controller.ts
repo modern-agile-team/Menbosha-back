7676acc28863a0db260fac93458d1843ece1a0ea
hobiJeong <jjb26433@naver.com>
2024-02-23 22:34:19 +0900

    doc(#251): create swagger api docs

--- src/admins/controllers/admins.controller.ts
+++ src/admins/controllers/admins.controller.ts
@@ -4,6 +4,7 @@ import {
   Controller,
   Param,
   Post,
+  Put,
   UseGuards,
   UseInterceptors,
   UsePipes,
@@ -12,11 +13,13 @@ import {
 import { ApiTags } from '@nestjs/swagger';
 import { BannedUsersService } from 'src/admins/banned-user/services/banned-users.service';
 import { OnlyAdmin } from 'src/admins/decorators/only-admin.decorator';
-import { CreateBannedUserBodyDto } from 'src/admins/dtos/create-banned-user-body.dto';
+import { CreateBannedUserBodyDto } from 'src/admins/banned-user/dtos/create-banned-user-body.dto';
 import { AdminHandlerGuard } from 'src/admins/guards/admin-handler.guard';
 import { AccessTokenAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
 import { GetUserId } from 'src/common/decorators/get-userId.decorator';
 import { SuccessResponseInterceptor } from 'src/common/interceptors/success-response.interceptor';
+import { ApiCreateBannedUser } from 'src/admins/swagger-decorators/create-banned-user.decorator';
+import { BannedUserDto } from 'src/admins/banned-user/dtos/banned-user.dto';
 
 @ApiTags('_admin')
 @UsePipes(
@@ -31,18 +34,24 @@ import { SuccessResponseInterceptor } from 'src/common/interceptors/success-resp
 export class AdminsController {
   constructor(private readonly bannedUsersService: BannedUsersService) {}
 
+  @ApiCreateBannedUser()
   @Post('users/:userId/banned-users')
   @OnlyAdmin(true)
   @UseGuards(AccessTokenAuthGuard, AdminHandlerGuard)
   createBannedUser(
     @Body() createBannedUserBodyDto: CreateBannedUserBodyDto,
     @Param('userId') userId: number,
-    @GetUserId() myId: number,
-  ) {
+    @GetUserId() adminId: number,
+  ): Promise<BannedUserDto> {
     return this.bannedUsersService.createBannedUser(
-      myId,
+      adminId,
       userId,
       createBannedUserBodyDto,
     );
   }
+
+  @Put('users/:userId')
+  @OnlyAdmin(true)
+  @UseGuards(AccessTokenAuthGuard, AdminHandlerGuard)
+  putUserUpdateStatus() {}
 }
