d8940aaddbceff588a1d39b50c111cf9a1072670
hobiJeong <jjb26433@naver.com>
2024-02-24 01:52:40 +0900

    setting(#251): add admin error code and message inside the error-related object

--- src/constants/error/error-code.constant.ts
+++ src/constants/error/error-code.constant.ts
@@ -1,7 +1,9 @@
+import { ADMIN_ERROR_CODE } from 'src/constants/error/admin/admin-error-code.constant';
 import { AUTH_ERROR_CODE } from './auth/auth-error-code.constant';
 import { COMMON_ERROR_CODE } from './common/common-error-code.constant';
 
 export const ERROR_CODE = {
   ...COMMON_ERROR_CODE,
   ...AUTH_ERROR_CODE,
+  ...ADMIN_ERROR_CODE,
 } as const;
