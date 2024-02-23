ea7f685814a00b76b4106659dcdd75c33f4753ca
hobiJeong <jjb26433@naver.com>
2024-02-23 22:33:09 +0900

    feat(#251): create repository save code

--- src/admins/banned-user/repositories/banned-user.repository.ts
+++ src/admins/banned-user/repositories/banned-user.repository.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { BannedUser } from 'src/admins/entities/banned-user.entity';
+import { CreateBannedUserBodyDto } from 'src/admins/banned-user/dtos/create-banned-user-body.dto';
+import { BannedUser } from 'src/admins/banned-user/entities/banned-user.entity';
 import { EntityManager } from 'typeorm';
 
 @Injectable()
 export class BannedUserRepository {
   constructor(private readonly entityManager: EntityManager) {}
 
-  createBannedUser() {
-    return this.entityManager.getRepository(BannedUser).save({});
+  createBannedUser(
+    entityManager: EntityManager,
+    adminId: number,
+    userId: number,
+    createBannedUserBodyDto: CreateBannedUserBodyDto,
+  ): Promise<BannedUser> {
+    return entityManager.getRepository(BannedUser).save({
+      banUserId: adminId,
+      bannedUserId: userId,
+      ...createBannedUserBodyDto,
+    });
   }
 }
