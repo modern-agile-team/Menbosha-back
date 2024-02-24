d1b69b9d7d5bc39985d66708065ae8e1bcf0d704
hobiJeong <jjb26433@naver.com>
2024-02-24 18:23:01 +0900

    feat(#251): create find api(incomplete), modify method name and import path

--- src/admins/banned-user/repositories/banned-user.repository.ts
+++ src/admins/banned-user/repositories/banned-user.repository.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { CreateBannedUserBodyDto } from 'src/admins/dtos/create-banned-user-body.dto';
-import { BannedUser } from 'src/admins/banned-user/entities/banned-user.entity';
+import { CreateBannedUserBodyDto } from '@src/admins/dtos/create-banned-user-body.dto';
+import { BannedUser } from '@src/admins/banned-user/entities/banned-user.entity';
 import { EntityManager } from 'typeorm';
 
 @Injectable()
 export class BannedUserRepository {
   constructor(private readonly entityManager: EntityManager) {}
 
-  createBannedUser(
+  create(
     entityManager: EntityManager,
     adminId: number,
     userId: number,
@@ -17,6 +17,21 @@ export class BannedUserRepository {
       banUserId: adminId,
       bannedUserId: userId,
       ...createBannedUserBodyDto,
+    });
+  }
+
+  findAllAndCount(
+    skip: number,
+    pageSize: number,
+    where: Record<string, any>,
+    order: Record<string, any>,
+  ) {
+    return this.entityManager.getRepository(BannedUser).findAndCount({
+      select: ['id', 'banUserId', 'bannedUserId', 'bannedAt', 'endAt'],
+      where,
+      order,
+      skip,
+      take: pageSize,
     });
   }
 }
