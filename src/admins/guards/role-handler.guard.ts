06b6a79f3272a04d7edfb9cc6533183b11ec3074
hobiJeong <jjb26433@naver.com>
2024-02-24 18:26:06 +0900

    refactor(#251): Separate guard's level into class and method

---
+++ src/admins/guards/role-handler.guard.ts
@@ -0,0 +1,32 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_TOKEN } from '@src/admins/constants/roles.token';
+import { ADMIN_ERROR_CODE } from '@src/constants/error/admin/admin-error-code.constant';
+import { AdminException } from '@src/http-exceptions/exceptions/admin-exception';
+import { UserRole } from '@src/users/constants/user-role.enum';
+import { UserService } from '@src/users/services/user.service';
+
+@Injectable()
+export class RoleHandlerGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly userService: UserService,
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    const { role } = await this.userService.findOneByOrNotFound({
+      where: { id: request.user.id },
+    });
+
+    if (!this.getMetadata(context).includes(role)) {
+      throw new AdminException({ code: ADMIN_ERROR_CODE.ADMIN_ONLY_ACCESS });
+    }
+
+    return this.getMetadata(context).includes(role);
+  }
+
+  private getMetadata(context: ExecutionContext) {
+    return this.reflector.get<UserRole[]>(ROLES_TOKEN, context.getHandler());
+  }
+}
