daf7fb3067992f52dfccae2e9256dbc59d666956
hobiJeong <jjb26433@naver.com>
2024-02-24 14:02:10 +0900

    setting(#251): setting import paths

--- src/admins/controllers/admins.controller.ts
+++ src/admins/controllers/admins.controller.ts
@@ -11,18 +11,18 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { BannedUsersService } from 'src/admins/banned-user/services/banned-users.service';
-import { OnlyAdmin } from 'src/admins/decorators/only-admin.decorator';
-import { CreateBannedUserBodyDto } from 'src/admins/dtos/create-banned-user-body.dto';
-import { AdminHandlerGuard } from 'src/admins/guards/admin-handler.guard';
-import { AccessTokenAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
-import { GetUserId } from 'src/common/decorators/get-userId.decorator';
-import { SuccessResponseInterceptor } from 'src/common/interceptors/success-response.interceptor';
-import { ApiCreateBannedUser } from 'src/admins/swagger-decorators/create-banned-user.decorator';
-import { BannedUserDto } from 'src/admins/banned-user/dtos/banned-user.dto';
-import { PutUpdateUserForAdminDto } from 'src/admins/dtos/put-update-user-for-admin.dto';
-import { AdminsService } from 'src/admins/services/admins.service';
-import { ApiPutUpdateUserStatus } from 'src/admins/swagger-decorators/put-update-user-status.decorator';
+import { BannedUserDto } from '@src/admins/banned-user/dtos/banned-user.dto';
+import { BannedUsersService } from '@src/admins/banned-user/services/banned-users.service';
+import { OnlyAdmin } from '@src/admins/decorators/only-admin.decorator';
+import { CreateBannedUserBodyDto } from '@src/admins/dtos/create-banned-user-body.dto';
+import { PutUpdateUserForAdminDto } from '@src/admins/dtos/put-update-user-for-admin.dto';
+import { AdminHandlerGuard } from '@src/admins/guards/admin-handler.guard';
+import { AdminsService } from '@src/admins/services/admins.service';
+import { ApiCreateBannedUser } from '@src/admins/swagger-decorators/create-banned-user.decorator';
+import { ApiPutUpdateUserStatus } from '@src/admins/swagger-decorators/put-update-user-status.decorator';
+import { AccessTokenAuthGuard } from '@src/auth/jwt/jwt-auth.guard';
+import { GetUserId } from '@src/common/decorators/get-userId.decorator';
+import { SuccessResponseInterceptor } from '@src/common/interceptors/success-response.interceptor';
 
 @ApiTags('_admin')
 @UsePipes(
