4d5a2c77488774e310d12ec693995decf7a54f80
hobiJeong <jjb26433@naver.com>
2024-02-26 14:02:04 +0900

    feat(#251): imports ReportsModule

--- src/admins/admins.module.ts
+++ src/admins/admins.module.ts
@@ -3,9 +3,10 @@ import { UserModule } from '@src/users/user.module';
 import { BannedUserModule } from '@src/admins/banned-user/banned-users.module';
 import { AdminsController } from '@src/admins/controllers/admins.controller';
 import { AdminsService } from '@src/admins/services/admins.service';
+import { ReportsModule } from '@src/reports/reports.module';
 
 @Module({
-  imports: [UserModule, BannedUserModule],
+  imports: [UserModule, BannedUserModule, ReportsModule],
   controllers: [AdminsController],
   providers: [AdminsService],
 })
