e0944452927b945dea514dd654321ad69b299cb6
hobiJeong <jjb26433@naver.com>
2024-03-08 03:08:09 +0900

    refactor(#325): setting constraints with user constant

--- src/users/dtos/get-my-intro.dto.ts
+++ src/users/dtos/get-my-intro.dto.ts
@@ -1,5 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { UserIntro } from '@src/entities/UserIntro';
+import {
+  USER_CAREER_LENGTH,
+  USER_CUSTOM_CATEGORY_LENGTH,
+  USER_DETAIL_LENGTH,
+  USER_PORTFOLIO_LENGTH,
+  USER_SHORT_INTRO_LENGTH,
+  USER_SNS_LENGTH,
+} from '@src/users/constants/user.constant';
 import { Exclude } from 'class-transformer';
 
 export class MyIntroDto implements Omit<UserIntro, 'user'> {
@@ -11,31 +19,43 @@ export class MyIntroDto implements Omit<UserIntro, 'user'> {
 
   @ApiProperty({
     description: '커스텀 카테고리',
+    minLength: USER_CUSTOM_CATEGORY_LENGTH.MIN,
+    maxLength: USER_CUSTOM_CATEGORY_LENGTH.MAX,
   })
   customCategory: string;
 
   @ApiProperty({
     description: '한 줄 소개',
+    minLength: USER_SHORT_INTRO_LENGTH.MIN,
+    maxLength: USER_SHORT_INTRO_LENGTH.MAX,
   })
   shortIntro: string;
 
   @ApiProperty({
     description: '경력',
+    minLength: USER_CAREER_LENGTH.MIN,
+    maxLength: USER_CAREER_LENGTH.MAX,
   })
   career: string;
 
   @ApiProperty({
     description: '세부 사항',
+    minLength: USER_DETAIL_LENGTH.MIN,
+    maxLength: USER_DETAIL_LENGTH.MAX,
   })
   detail: string;
 
   @ApiProperty({
     description: '포트폴리오',
+    minLength: USER_PORTFOLIO_LENGTH.MIN,
+    maxLength: USER_PORTFOLIO_LENGTH.MAX,
   })
   portfolio: string;
 
   @ApiProperty({
     description: 'SNS',
+    minLength: USER_SNS_LENGTH.MIN,
+    maxLength: USER_SNS_LENGTH.MAX,
   })
   sns: string;
 
--- src/users/dtos/update-user-intro-dto.ts
+++ src/users/dtos/update-user-intro-dto.ts
@@ -1,54 +1,75 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsPositiveInt } from '@src/common/decorators/validators/is-positive-int.decorator';
+import {
+  USER_CAREER_LENGTH,
+  USER_CUSTOM_CATEGORY_LENGTH,
+  USER_DETAIL_LENGTH,
+  USER_PORTFOLIO_LENGTH,
+  USER_SHORT_INTRO_LENGTH,
+  USER_SNS_LENGTH,
+} from '@src/users/constants/user.constant';
+import { IsBoolean, IsOptional, Length } from 'class-validator';
 
 export class UpdateUserIntroDTO {
   @ApiPropertyOptional({
     example: '안녕하세요',
     description: '한 줄 소개',
+    minLength: USER_SHORT_INTRO_LENGTH.MIN,
+    maxLength: USER_SHORT_INTRO_LENGTH.MAX,
   })
   @IsOptional()
-  @IsString()
+  @Length(USER_SHORT_INTRO_LENGTH.MIN, USER_SHORT_INTRO_LENGTH.MAX)
   shortIntro?: string;
 
   @ApiPropertyOptional({
     example: '숨쉬기 경력 20년',
     description: '경력',
+    minLength: USER_CAREER_LENGTH.MIN,
+    maxLength: USER_CAREER_LENGTH.MAX,
   })
   @IsOptional()
-  @IsString()
+  @Length(USER_CAREER_LENGTH.MIN, USER_CAREER_LENGTH.MAX)
   career?: string;
 
   @ApiPropertyOptional({
     example: '코로 숨쉬기, 입으로 숨쉬기',
     description: '커스텀 카테고리',
+    minLength: USER_CUSTOM_CATEGORY_LENGTH.MIN,
+    maxLength: USER_CUSTOM_CATEGORY_LENGTH.MAX,
   })
   @IsOptional()
-  @IsString()
+  @Length(USER_CUSTOM_CATEGORY_LENGTH.MIN, USER_CUSTOM_CATEGORY_LENGTH.MAX)
   customCategory?: string;
 
   @ApiPropertyOptional({
     example:
       '안녕하세요. 저는 트위치에서 방송을 하고 있는 스트리머 케인입니다.',
     description: '상세 소개(옵션)',
+    minLength: USER_DETAIL_LENGTH.MIN,
+    maxLength: USER_DETAIL_LENGTH.MAX,
   })
   @IsOptional()
-  @IsString()
+  @Length(USER_DETAIL_LENGTH.MIN, USER_DETAIL_LENGTH.MAX)
   detail?: string;
 
   @ApiPropertyOptional({
     example: 'https://www.naver.com',
     description: '포트폴리오 링크',
+    minLength: USER_PORTFOLIO_LENGTH.MIN,
+    maxLength: USER_PORTFOLIO_LENGTH.MAX,
   })
   @IsOptional()
-  @IsString()
+  @Length(USER_PORTFOLIO_LENGTH.MIN, USER_PORTFOLIO_LENGTH.MAX)
   portfolio?: string;
 
   @ApiPropertyOptional({
     example: 'https://www.naver.com',
     description: 'sns 링크',
+    minLength: USER_SNS_LENGTH.MIN,
+    maxLength: USER_SNS_LENGTH.MAX,
   })
   @IsOptional()
-  @IsString()
+  @Length(USER_SNS_LENGTH.MIN, USER_SNS_LENGTH.MAX)
   sns?: string;
 
   @ApiPropertyOptional({
@@ -56,7 +77,7 @@ export class UpdateUserIntroDTO {
     description: '희망 카테고리 id',
   })
   @IsOptional()
-  @IsNumber()
+  @IsPositiveInt()
   hopeCategoryId?: number;
 
   @ApiPropertyOptional({
@@ -64,7 +85,7 @@ export class UpdateUserIntroDTO {
     description: '활동 카테고리 id',
   })
   @IsOptional()
-  @IsNumber()
+  @IsPositiveInt()
   activityCategoryId?: number;
 
   @ApiPropertyOptional({
