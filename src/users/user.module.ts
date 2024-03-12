453d69bf9047e5bcb4b0d7fad9a7ff25022ac416
hobiJeong <jjb26433@naver.com>
2024-03-12 20:27:50 +0900

    refactor(#343): remove s3 service at providers and import s3 module

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,6 +1,5 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { S3Service } from '@src/common/s3/s3.service';
 import { AuthModule } from '@src/auth/auth.module';
 import { TotalCountModule } from '@src/total-count/total-count.module';
 import { UserImageController } from '@src/users/controllers/user-image.controller';
@@ -16,17 +15,18 @@ import { UserIntroService } from '@src/users/services/user-intro-service';
 import { UserRankingService } from '@src/users/services/user-ranking.service';
 import { UserService } from '@src/users/services/user.service';
 import { User } from '@src/entities/User';
+import { S3Module } from '@src/common/s3/s3.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     forwardRef(() => AuthModule),
     TotalCountModule,
+    S3Module,
   ],
   controllers: [UserController, UserImageController],
   providers: [
     UserIntroService,
-    S3Service,
     UserRepository,
     UserImageRepository,
     UserService,
