9299522e294ad35718d7620b11ae72dcd7a1750a
hobiJeong <jjb26433@naver.com>
2024-03-12 20:29:37 +0900

    refactor(#343): use app config service instead of dotenv

--- src/http-exceptions/services/http-exception.service.ts
+++ src/http-exceptions/services/http-exception.service.ts
@@ -1,11 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { ERROR_CODE } from '@src/constants/error/error-code.constant';
 import { ERROR_MESSAGE } from '@src/constants/error/error-message.constant';
+import { AppConfigService } from '@src/core/app-config/services/app-config.service';
 import { ExceptionResponseDto } from '@src/http-exceptions/dto/exception-response.dto';
 import { ValueOf } from '@src/type/type';
-import { config } from 'dotenv';
-
-config();
 
 export interface ExceptionError {
   code: ValueOf<typeof ERROR_CODE>;
@@ -15,10 +13,13 @@ export interface ExceptionError {
 
 @Injectable()
 export class HttpExceptionService {
+  constructor(private readonly appConfigService: AppConfigService) {}
+
   buildResponseJson(
     statusCode: number,
     exceptionError: ExceptionError,
   ): ExceptionResponseDto {
+    const isProduction = this.appConfigService.isProduction();
     const code = exceptionError.code;
 
     return new ExceptionResponseDto({
@@ -26,9 +27,7 @@ export class HttpExceptionService {
       code,
       message: ERROR_MESSAGE[code],
       stack:
-        statusCode >= 500 && process.env.NODE_ENV
-          ? exceptionError.stack
-          : undefined,
+        statusCode >= 500 && isProduction ? exceptionError.stack : undefined,
       additionalResponse: exceptionError.additionalResponse,
     });
   }
