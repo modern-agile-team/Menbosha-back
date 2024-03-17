d68000b616941347a3b728caf3fdb96ee2876a28
2swo <swlee456@naver.com>
2024-03-18 00:01:34 +0900

    refactor(#317) helpMeBoardImage decorator body

--- src/boards/swagger-decorators/helpMeBoard/patch-help-me-board-images-decorators.ts
+++ src/boards/swagger-decorators/helpMeBoard/patch-help-me-board-images-decorators.ts
@@ -1,6 +1,7 @@
 import { applyDecorators } from '@nestjs/common';
 import {
   ApiBearerAuth,
+  ApiBody,
   ApiOperation,
   ApiParam,
   ApiResponse,
@@ -80,5 +81,16 @@ export function ApiUpdateHelpMeBoardImage() {
     }),
     ApiBearerAuth('access-token'),
     ApiParam({ name: 'helpMeBoardId', example: 1 }),
+    ApiBody({
+      schema: {
+        type: 'form-data',
+        properties: {
+          files: { type: 'file' },
+        },
+        example: {
+          files: '고양이.png',
+        },
+      },
+    }),
   );
 }
